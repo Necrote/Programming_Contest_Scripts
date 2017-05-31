698bd3e220e871ee7fe6e15415e711480fc05057
Vivek Patel <Necrote@users.noreply.github.com>
2017-05-31 13:57:49 +0530

    Update gen.cpp

--- gen.cpp
+++ gen.cpp
@@ -1,27 +1,11 @@
-/*
-* It is another type of generators. It writes several files named
-* as test indices.
-*
-* For example, this generator writes 10 files (tests) from 1 to 10.
-* This type of generators supported by Polygon too, but I believe
-* that stdout-generators are more preferred.
-*
-* The generator for A+B problem, generates 10 tests where each
-* number is between 1 and 100, and tests grow with indices.
-*/
 #include "testlib.h"
 #include <iostream>
+
 using namespace std;
-void writeTest(int test)
-{
-	startTest(test);
-	cout << rnd.next(1, test * test)
-	<< " " << rnd.next(1, test * test) << endl;
-}
+
 int main(int argc, char* argv[])
 {
-	registerGen(argc, argv, 1);
-	for (int i = 1; i <= 10; i++)
-		writeTest(i);
-	return 0;
+    registerGen(argc, argv, 1);
+    cout << rnd.next(1, 10) << endl; /* Random number in the range [1,10]. */
+    cout << rnd.next("[a-zA-Z0-9]{1,1000}") << endl;  /* Random word of length [1,1000]. */
 }
